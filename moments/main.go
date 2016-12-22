468c57a928893c730f4522fb29ed4e8873a6c951
Alex Nichol <unixpickle@gmail.com>
2016-12-22 14:48:28 -0500

    small tool to figure out average word probability
    I wanted to know what the probability was of my LSTM predicting the next word. Such knowledge will be important for the UI, since the UI makes things red/blue based on how likely/unlikely the word was.
    
    Mean: 0.044758569430310056
    Var:  0.008328815171037824

---
+++ moments/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/unixpickle/textpect/analyzer"
+	"github.com/unixpickle/weakai/rnn"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		die("Usage: moments <net> <corpus_dir>")
+	}
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		die(err)
+	}
+	net, err := rnn.DeserializeStackedBlock(data)
+	if err != nil {
+		die(err)
+	}
+	listing, err := ioutil.ReadDir(os.Args[2])
+	if err != nil {
+		die(err)
+	}
+	count := 0
+	sum := 0.0
+	sqSum := 0.0
+	for _, x := range listing {
+		if x.IsDir() || strings.HasPrefix(x.Name(), ".") {
+			continue
+		}
+		contents, err := ioutil.ReadFile(filepath.Join(os.Args[2], x.Name()))
+		if err != nil {
+			die(err)
+		}
+		c, s, s2 := wordProbabilities(net, string(contents))
+		count += c
+		sum += s
+		sqSum += s2
+	}
+	mean := sum / float64(count)
+	variance := sqSum/float64(count) - mean*mean
+	fmt.Println("Mean:", mean)
+	fmt.Println("Var: ", variance)
+}
+
+func wordProbabilities(b rnn.Block, text string) (count int, sum, sqSum float64) {
+	a := analyzer.NewAnalyzer(b, text)
+	for !a.Done() {
+	}
+	for _, t := range a.Tokens() {
+		if t.Type == analyzer.Word {
+			sum += t.Probability
+			sqSum += t.Probability * t.Probability
+			count++
+		}
+	}
+	return
+}
+
+func die(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+	os.Exit(1)
+}
