b84ea712e4425d7482d81eb64ed17022449fa466
Alex Nichol <unixpickle@gmail.com>
2016-12-24 16:31:12 -0500

    cache suggestions for faster sampling

--- analyzer/suggestions.go
+++ analyzer/suggestions.go
@@ -13,8 +13,9 @@ import (
 func suggestions(last byte, s rnn.State, b rnn.Block) ([]string, []float64) {
 	seen := map[string]bool{}
 	samps := samples{}
+	sampler := newWordSampler(last, s, b)
 	for i := 0; i < 100; i++ {
-		str, prob := sampleWord(last, s, b)
+		str, prob := sampler.Sample()
 		if _, ok := seen[str]; !ok {
 			seen[str] = true
 			samps = append(samps, sample{str, prob})
@@ -30,29 +31,65 @@ func suggestions(last byte, s rnn.State, b rnn.Block) ([]string, []float64) {
 	return strs, probs
 }
 
-func sampleWord(last byte, s rnn.State, b rnn.Block) (string, float64) {
+type cacheEntry struct {
+	State rnn.State
+	Dist  linalg.Vector
+}
+
+type wordSampler struct {
+	Last  byte
+	Block rnn.Block
+
+	Cache map[string]cacheEntry
+}
+
+func newWordSampler(last byte, s rnn.State, b rnn.Block) *wordSampler {
 	lastIn := make(linalg.Vector, 0x100)
 	lastIn[int(last)] = 1
-	prob := 1.0
-	word := ""
+	v := &autofunc.Variable{Vector: lastIn}
+	out := b.ApplyBlock([]rnn.State{s}, []autofunc.Result{v})
+	return &wordSampler{
+		Last:  last,
+		Block: b,
+		Cache: map[string]cacheEntry{
+			"": {
+				State: out.States()[0],
+				Dist:  out.Outputs()[0],
+			},
+		},
+	}
+}
+
+func (w *wordSampler) Sample() (word string, prob float64) {
+	prob = 1.0
+	var lastState rnn.State
 	for {
-		v := &autofunc.Variable{Vector: lastIn}
-		out := b.ApplyBlock([]rnn.State{s}, []autofunc.Result{v})
-		s = out.States()[0]
-		ch := sampleChar(out.Outputs()[0])
+		entry, ok := w.Cache[word]
+		if !ok {
+			inVec := make(linalg.Vector, 0x100)
+			inVec[word[len(word)-1]] = 1
+			v := &autofunc.Variable{Vector: inVec}
+			out := w.Block.ApplyBlock([]rnn.State{lastState}, []autofunc.Result{v})
+			entry = cacheEntry{
+				State: out.States()[0],
+				Dist:  out.Outputs()[0],
+			}
+			w.Cache[word] = entry
+		}
+		lastState = entry.State
+		ch := sampleChar(entry.Dist)
 		for len(word) == 0 && (ch == ' ' || ch == '\n') {
-			ch = sampleChar(out.Outputs()[0])
+			ch = sampleChar(entry.Dist)
 			continue
 		}
-		prob *= math.Exp(out.Outputs()[0][ch])
 		if ch == ' ' || ch == '\n' {
+			prob *= math.Exp(entry.Dist[' ']) + math.Exp(entry.Dist['\n'])
 			break
 		}
-		word += string(byte(ch))
-		lastIn = make(linalg.Vector, 0x100)
-		lastIn[ch] = 1
+		prob *= math.Exp(entry.Dist[ch])
+		word += string([]byte{byte(ch)})
 	}
-	return word, prob
+	return
 }
 
 func sampleChar(v linalg.Vector) int {
